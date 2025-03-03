d7b54181da0a82c7afc0e790f4d8635f65f9b1c5
nithish365 <nithish5252@gmail.com>
2025-03-03 15:19:46 +0530

    sp;line added

---
+++ wire harness project/Assets/Scripts/DynamicSplineUpdater.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Splines;
+using System.Collections.Generic;
+
+public class DynamicSplineUpdater : MonoBehaviour
+{
+    public SplineContainer splineContainer;
+    public List<Transform> spheres = new List<Transform>(); // List of sphere instances
+
+    void Update()
+    {
+        UpdateSpline(); // Continuously update spline
+    }
+
+    void UpdateSpline()
+    {
+        if (splineContainer == null || spheres.Count == 0) return;
+
+        Spline spline = splineContainer.Spline;
+        spline.Clear();
+
+        foreach (Transform sphere in spheres)
+        {
+            spline.Add(new BezierKnot(sphere.position)); // Add sphere positions to spline
+        }
+
+        ApplySmoothTangents(spline);
+
+
+    }
+
+    void ApplySmoothTangents(Spline spline)
+    {
+        for (int i = 0; i < spline.Count; i++)
+        {
+            spline.SetTangentMode(i, TangentMode.AutoSmooth);
+        }
+    }
+
+    public void AddSphere(Transform sphere)
+    {
+        if (!spheres.Contains(sphere))
+        {
+            spheres.Add(sphere);
+        }
+    }
+
+    public void RemoveSphere(Transform sphere)
+    {
+        if (spheres.Contains(sphere))
+        {
+            spheres.Remove(sphere);
+        }
+    }
+}
---
+++ wire harness project/Assets/Scripts/SphereSpawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SphereSpawner : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public DynamicSplineUpdater splineUpdater;
+    public int sphereCount = 5;
+
+    void Start()
+    {
+        for (int i = 0; i < sphereCount; i++)
+        {
+            Vector3 randomPosition = new Vector3(i * 3, Random.Range(0, 5), Random.Range(-5, 5));
+            GameObject newSphere = Instantiate(spherePrefab, randomPosition, Quaternion.identity);
+            splineUpdater.AddSphere(newSphere.transform);
+        }
+    }
+}
